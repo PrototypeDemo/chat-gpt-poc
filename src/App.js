c7143a01b875c438807c762d954afc7a3238719c
prabhuteja.katkam_csgi <prabhuteja.katakam@csgi.com>
2023-03-30 08:43:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "@fontsource/roboto"; // Loading Roboto font. MUI was designed with this font in mind.
+import {
+  Switch,
+  FormGroup,
+  FormControlLabel,
+  CssBaseline,
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MessageIcon from "@mui/icons-material/Message";
+import QuillEditor from "./QuillEditor/QuillEditor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Define custom palette colors
+const customColors = {
+  primary: {
+    main: "#861DDD",
+  },
+  primaryLight: {
+    main: "#EDDDF6",
+  },
+  secondary: {
+    main: "#665A6F",
+  },
+  error: {
+    main: "#ba1b1b",
+  },
+  charcoal: {
+    main: "#333333",
+  },
+};
+
+// Define theme settings with custom colors
+const light = createTheme({
+  palette: {
+    mode: "light",
+    ...customColors,
+  },
+});
+
+const dark = createTheme({
+  palette: {
+    mode: "dark",
+    ...customColors,
+  },
+});
+
+const App = () => {
+  // Load the theme preference from local storage
+  const [isDarkTheme, setIsDarkTheme] = useState(
+    localStorage.getItem("isDarkTheme") === "true"
   );
-}
+
+  // This function is triggered when the Switch component is toggled
+  const changeTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  // Save the theme preference to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("isDarkTheme", isDarkTheme);
+  }, [isDarkTheme]);
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? dark : light}>
+      <CssBaseline />
+
+      <div className="App">
+        <Box>
+          <AppBar position="static">
+            <Toolbar color="primary">
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MessageIcon />
+              </IconButton>
+              <Typography variant="h6">ChatGPT-POC</Typography>
+              <FormGroup className="theme-btn">
+                <FormControlLabel
+                  control={
+                    <Switch checked={isDarkTheme} onChange={changeTheme} />
+                  }
+                  label="Dark Theme"
+                />
+              </FormGroup>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <QuillEditor />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ src/QuillEditor/QuillEditor.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import QuillEditorToolBar, {
+  modules,
+  formats,
+} from "../QuillEditorToolBar/QuillEditorToolBar";
+import "react-quill/dist/quill.snow.css";
+
+import { Container } from "@mui/system";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import SendIcon from "@mui/icons-material/Send";
+import "./QuillEditor.css";
+import axios from "axios";
+
+function QuillEditor() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleEditorChange = (content, delta, source, editor) => {
+    setResponse(editor.getHTML());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    axios
+      .post("http://localhost:5555/chat", { prompt })
+      .then((res) => {
+        setResponse(res.data);
+        // split response into an array of words
+        // const words = res.data.split(" ");
+
+        // // iterate over the array of words and display them one by one
+        // words.forEach((word, index) => {
+        //   setTimeout(() => {
+        //     //setResponse((prevValue) => `${prevValue}` + " " + `${word}`);
+        //     setResponse((prevValue) => prevValue + " " + word);
+        //   }, index * 500);
+        // });
+
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <Card className="quill-card">
+        <CardContent className="quill-card-content">
+          <form onSubmit={handleSubmit}>
+            <div className="m-b-15">
+              <TextField
+                label="Write your question here and click send icon"
+                placeholder="Enter your question..."
+                multiline
+                fullWidth
+                maxRows={2}
+                id="prompt"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="generate answer"
+                        edge="end"
+                        color="primary"
+                        type="submit"
+                      >
+                        <SendIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                onKeyPress={(event) => {
+                  if (event.key === "Enter" && !event.shiftKey) {
+                    event.preventDefault();
+                    handleSubmit(event);
+                  }
+                }}
+              />
+            </div>
+          </form>
+          <div className="text-editor">
+            <QuillEditorToolBar />
+            <ReactQuill
+              theme="snow"
+              value={loading ? "loading..." : response}
+              onChange={handleEditorChange}
+              placeholder={"Answer will come here..."}
+              modules={modules}
+              formats={formats}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
+
+export default QuillEditor;
